2b72d493f17f3b008e6d343e54244973f71753cd
zubairhmd <zubairhmd84@gmail.com>
2021-09-01 14:01:06 +0600

    final commit

---
+++ conceptual.js
@@ -0,0 +1,44 @@
+/*
+1. user visit korbe
+2. user kichu search dibe
+3. amar search result dekhabe
+4. button e click korbe amar details dekhabe
+*/
+
+//TODO - how to proccess
+/*
+1. call search field ID
+2. call search button ID
+3. Add event Listner on the button
+4. click korle ekta Function run korbe jekhan theke search box er VALUE gulo nibe
+5. tarpor ekta variable niye oitar moddhe search er input value nite hobe
+6. then API
+
+[ {} {} {} {} {} {}] - for loop use korbo
+
+.then(data => consol.log(data))  - eta holo arrow function
+its look like
+.then((data => {
+    consol.log(data)
+})
+
+
+DISPLAY SHOW
+display moddhe jodi [] array na hoye object akare shoow kore tahole for loop, for each kichui chalano jabe na .
+
+http://prntscr.com/1qw2xfr
+https://prnt.sc/1qw1whe
+https://prnt.sc/1qvyz23
+
+
+*/
+
+const first = 2;
+const second = '2';
+if (first === second) {
+    console.log('Its true');
+}
+else {
+    console.log('false');
+}
+
